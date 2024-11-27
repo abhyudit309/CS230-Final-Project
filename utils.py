b5c8aabb90d6916a8614695c7cbf547eaf7fd8c5
Ubuntu <ubuntu@ip-172-31-43-159.us-west-2.compute.internal>
2024-11-27 00:31:39 +0000

    data augmentation and dropout

--- utils.py
+++ utils.py
@@ -13,9 +13,15 @@ def initialize_new_head(layer: nn.Linear):
     nn.init.zeros_(layer.bias)
 
 
-def prepare_model_for_finetune(model_name: str, num_classes: int) -> VisionTransformer:
+def prepare_model_for_finetune(model_name: str, num_classes: int, use_dropout: bool = False) -> VisionTransformer:
     teacher_model: VisionTransformer = create_model(model_name, pretrained=True)
-    teacher_model.head = nn.Linear(teacher_model.head.in_features, num_classes)
+    if use_dropout:
+        teacher_model.head = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(teacher_model.head.in_features, num_classes)
+        )
+    else:
+        teacher_model.head = nn.Linear(teacher_model.head.in_features, num_classes)
     teacher_model.head.apply(initialize_new_head)
 
     for param_name, param in teacher_model.named_parameters():
