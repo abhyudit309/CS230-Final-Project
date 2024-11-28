b1701bce5fb6f16634c1256aaef203ac8497f1dc
Ubuntu <ubuntu@ip-172-31-43-159.us-west-2.compute.internal>
2024-11-28 03:30:50 +0000

    knowledge distillation

--- knowledge_distillation.py
+++ knowledge_distillation.py
@@ -1,82 +1,164 @@
+import typing as T
+import numpy as np
+
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data.dataloader
+import torch.utils.data as data
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torchvision import datasets, transforms
-from timm import create_model
 
-def count_parameters(model: nn.Module):
-    return sum(param.numel() for param in model.parameters() if param.requires_grad)
+from tqdm import tqdm
+from timm.models.mobilenetv3 import MobileNetV3
+from timm.models.vision_transformer import VisionTransformer
 
-teacher_model = create_model('vit_base_patch16_224', pretrained=True)
-teacher_model.eval()
+from utils import count_parameters, prepare_student_for_training, load_teacher_for_distillation
 
-student_model_mobilenet = create_model('mobilenetv3_small_100', pretrained=False, num_classes=1000)
 
-student_model_vit = create_model('deit_tiny_patch16_224', pretrained=False, num_classes=1000)
+def log_metrics(epoch: int, lr: float, loss: float, accuracy: float, file_path: str) -> None:
+    with open(file_path, 'a') as file:
+        file.write(f'Epoch: {epoch + 1}, LR => {lr:.6f}, Loss => {loss:.4f}, Accuracy => {accuracy:.2f}%\n')
 
-print(type(teacher_model), type(student_model_mobilenet), type(student_model_vit))
-print(f"Num of param in teacher => {count_parameters(teacher_model) / 1e6}M")
-print(f"Num of param in student mobilenet => {count_parameters(student_model_mobilenet) / 1e6}M")
-print(f"Num of param in student vit => {count_parameters(student_model_vit) / 1e6}M")
 
-def distillation_loss(logits_student, logits_teacher, temperature=3.0):
-    p_teacher = nn.functional.softmax(logits_teacher / temperature, dim=1)
-    p_student = nn.functional.log_softmax(logits_student / temperature, dim=1)
-    loss = nn.functional.kl_div(p_student, p_teacher, reduction='batchmean') * (temperature ** 2)
-    return loss
+def validate_student(model: T.Union[MobileNetV3, VisionTransformer], val_loader: data.DataLoader) -> T.Tuple[float, float]:
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
 
-def feature_distillation_loss(student_features, teacher_features):
-    loss = nn.functional.mse_loss(student_features, teacher_features)
-    return loss
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+            logits = model(images)
+            loss = F.cross_entropy(logits, labels)
+            val_loss += loss.item()
 
-train_dataset = datasets.CIFAR100(root='./data', train=True, transform=transform, download=True)
-val_dataset = datasets.CIFAR100(root='./data', train=False, transform=transform, download=True)
+            pred = torch.argmax(logits, dim=1)
+            total += labels.size(0)
+            correct += (pred == labels).sum().item()
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=2)
+    avg_val_loss = val_loss / len(val_loader)
+    val_accuracy = 100 * correct / total
+    return avg_val_loss, val_accuracy
 
-# training
-student_model = student_model_mobilenet
-student_model = student_model.to('cpu')
-teacher_model = teacher_model.to('cpu')
 
-optimizer = optim.Adam(student_model.parameters(), lr=0.001)
-criterion = nn.CrossEntropyLoss()
+def train_student(
+    teacher_model: VisionTransformer,
+    student_model: T.Union[MobileNetV3, VisionTransformer],
+    train_loader: data.DataLoader,
+    val_loader: data.DataLoader,
+    learning_rate: float,
+    weight_decay: float,
+    epochs: int,
+    temperature: float,
+    distillation_weight: float,
+    val_freq: int,
+    train_log_file: str = './student_logs/training_log_v1.txt',
+    val_log_file: str = './student_logs/validation_log_v1.txt',
+    save_path: str = './student_models/trained_student_v1.pth',
+    final_path: str = './student_models/trained_student_v1_final.pth',
+) -> None:
+    assert 0.0 <= distillation_weight <= 1.0, 'Distillation weight should be in the range [0, 1]!'
+    assert epochs % val_freq == 0, "Total epochs should be divisible by validation frequency!"
 
-def train_distillation(teacher: nn.Module, student: nn.Module, train_loader, optimizer, criterion, temperature=3.0, epochs=10):
-    teacher.eval()
-    student.train()
+    teacher_model.eval()
+    best_val_loss = np.inf
+    optimizer = optim.AdamW(student_model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    scheduler = CosineAnnealingLR(optimizer=optimizer, T_max=epochs)
 
     for epoch in range(epochs):
+        student_model.train()
         running_loss = 0.0
+        correct = 0
+        total = 0
 
-        for images, labels in train_loader:
-            images, labels = images.to('cpu'), labels.to('cpu')
+        progress_bar = tqdm(train_loader, desc=f'Epoch {epoch + 1}/{epochs}', unit='batch')
+        for images, labels in progress_bar:
+            images, labels = images.to(device), labels.to(device)
 
             with torch.no_grad():
-                teacher_logits = teacher(images)
-            student_logits = student(images)
+                teacher_logits = teacher_model(images)
+            student_logits = student_model(images)
 
-            loss_distillation = distillation_loss(student_logits, teacher_logits, temperature)
-            loss_task = nn.functional.cross_entropy(student_logits, labels)
-            print("1", teacher_logits.shape)
-            print("2", student_logits.shape)
-            print("3", labels.shape)
-            loss = loss_distillation + loss_task
+            p_teacher = F.softmax(teacher_logits / temperature, dim=1)
+            p_student = F.log_softmax(student_logits / temperature, dim=1)
+            distillation_loss = F.kl_div(p_student, p_teacher, reduction='batchmean') * (temperature ** 2)
+
+            task_loss = F.cross_entropy(student_logits, labels)
+
+            loss = distillation_weight * distillation_loss + (1.0 - distillation_weight) * task_loss
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
+            progress_bar.set_postfix(loss=loss.item())
 
-        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(train_loader):.4f}')
+            pred = torch.argmax(student_logits, dim=1)
+            total += labels.size(0)
+            correct += (pred == labels).sum().item()
 
-train_distillation(teacher_model, student_model, train_loader, optimizer, criterion)   
+        current_lr = scheduler.get_last_lr()[0]
+        scheduler.step()
+        
+        avg_loss = running_loss / len(train_loader)
+        accuracy = 100 * correct / total
+        print(f'Epoch [{epoch + 1}/{epochs}]: Training Loss => {avg_loss:.4f}, Accuracy => {accuracy:.2f}%\n')
+        log_metrics(epoch, current_lr, avg_loss, accuracy, train_log_file)
+
+        if (epoch + 1) % val_freq == 0:
+            avg_val_loss, val_accuracy = validate_student(student_model, val_loader)
+            print(f'Evaluation results for Epoch [{epoch + 1}/{epochs}]:')
+            print(f'Validation Loss => {avg_val_loss:.4f}')
+            print(f'Validation accuracy => {val_accuracy:.2f}%\n')
+            log_metrics(epoch, current_lr, avg_val_loss, val_accuracy, val_log_file)
+
+            # Checkpoint model
+            if avg_val_loss < best_val_loss:
+                print(f'Overwriting older model [val loss = {best_val_loss:.4f}] with model [val loss = {avg_val_loss:.4f}] at Epoch {epoch + 1}/{epochs}\n')
+                best_val_loss = avg_val_loss
+                torch.save(student_model.state_dict(), save_path)
+
+        # Save final model as well
+        if epoch + 1 == epochs:
+            torch.save(student_model.state_dict(), final_path)
+
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Loading dataset
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    train_dataset = datasets.CIFAR100(root='./data', train=True, transform=transform, download=True)
+    val_dataset = datasets.CIFAR100(root='./data', train=False, transform=transform, download=True)
+
+    train_loader = data.DataLoader(train_dataset, batch_size=512, shuffle=True, num_workers=4)
+    val_loader = data.DataLoader(val_dataset, batch_size=512, shuffle=False, num_workers=4)
+
+    # Train Student Model
+    teacher_model = load_teacher_for_distillation('vit_base_patch16_224', num_classes=100, path='./teacher_models/fine_tuned_teacher_v5_final.pth').to(device)
+    student_model = prepare_student_for_training('deit_tiny_patch16_224', num_classes=100).to(device)
+
+    print(f'Number of parameters in teacher => {count_parameters(teacher_model) / 1e6}M')
+    print(f'Number of parameters in student => {count_parameters(student_model) / 1e6}M')
+
+    train_student(
+        teacher_model,
+        student_model,
+        train_loader,
+        val_loader, 
+        learning_rate=1e-3, 
+        weight_decay=1e-4, 
+        epochs=3,
+        temperature=3.0,
+        distillation_weight=0.5,
+        val_freq=1,
+    )
+
+    print('Training complete and models saved!')
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+import typing as T
+
+import torch
 import torch.nn as nn
 from timm import create_model
+from timm.models.mobilenetv3 import MobileNetV3
 from timm.models.vision_transformer import VisionTransformer
 
 
@@ -13,7 +17,7 @@ def initialize_new_head(layer: nn.Linear):
         nn.init.zeros_(layer.bias)
 
 
-def prepare_model_for_finetune(model_name: str, num_classes: int, use_dropout: bool = False) -> VisionTransformer:
+def prepare_teacher_for_finetune(model_name: str, num_classes: int, use_dropout: bool = False) -> VisionTransformer:
     teacher_model: VisionTransformer = create_model(model_name, pretrained=True)
     if use_dropout:
         teacher_model.head = nn.Sequential(
@@ -28,4 +32,24 @@ def prepare_model_for_finetune(model_name: str, num_classes: int, use_dropout: b
         if 'head' not in param_name:
             param.requires_grad = False
 
+    return teacher_model
+
+
+def prepare_student_for_training(model_name: str, num_classes: int) -> T.Union[MobileNetV3, VisionTransformer]:
+    student_model: T.Union[MobileNetV3, VisionTransformer] = create_model(model_name, pretrained=False)
+    if isinstance(student_model, MobileNetV3):
+        student_model.classifier = nn.Linear(student_model.classifier.in_features, num_classes)
+        student_model.classifier.apply(initialize_new_head)
+    else:
+        student_model.head = nn.Linear(student_model.head.in_features, num_classes)
+        student_model.head.apply(initialize_new_head)
+
+    return student_model
+
+
+def load_teacher_for_distillation(model_name: str, num_classes: int, path: str) -> VisionTransformer:
+    teacher_model: VisionTransformer = create_model(model_name, pretrained=False)
+    teacher_model.head = nn.Linear(teacher_model.head.in_features, num_classes)
+
+    teacher_model.load_state_dict(torch.load(path, weights_only=True))
     return teacher_model
